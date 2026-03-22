f7c764dbefc20cabcca9b8e1257c728f448dc1e6
rachelicr <rachel.alcraft@icr.ac.uk>
2026-03-22 17:28:49 +0000

    Added a version function

--- src/map_plane/__init__.py
+++ src/map_plane/__init__.py
@@ -2,8 +2,11 @@ import os
 from pathlib import Path
 
 __version__ = "0.1.0"
-__git_sha__ = "634062d"  # updated automatically by pre-commit hook
-__git_date__ = "2026-03-22"  # updated automatically by pre-commit hook
+__git_sha__ = "affd704"  # updated automatically by pre-commit hook
+__git_datetime__ = "22-03-2026 17:28"  # updated automatically by pre-commit hook
+
+def version():
+    return f"{__version__}+{__git_sha__} ({__git_datetime__})"
 
 # Global data directory setting
 DEFAULT_DATA_DIR = Path.home() / ".map_plane" / "data"
