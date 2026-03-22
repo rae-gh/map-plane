a39691866b5e8b16246e82c05b79594fd31a3560
rachelicr <rachel.alcraft@icr.ac.uk>
2026-03-22 17:21:56 +0000

    Additional test of pre-commit hook

--- src/map_plane/__init__.py
+++ src/map_plane/__init__.py
@@ -2,11 +2,9 @@ import os
 from pathlib import Path
 
 __version__ = "0.1.0"
-__git_sha__ = "d43d1a8"  # updated automatically by pre-commit hook
+__git_sha__ = "ca7e4d2"  # updated automatically by pre-commit hook
 
 # Global data directory setting
 DEFAULT_DATA_DIR = Path.home() / ".map_plane" / "data"
 MPDATA_DIR = Path(os.environ.get("MPDATA_DIR", DEFAULT_DATA_DIR))
 MPDATA_DIR.mkdir(parents=True, exist_ok=True)
-
-# Subpackage imports
