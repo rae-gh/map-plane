634062d7a1857eb31f107155bed250e2b330e0cb
rachelicr <rachel.alcraft@icr.ac.uk>
2026-03-22 17:23:12 +0000

    Added a date too

--- src/map_plane/__init__.py
+++ src/map_plane/__init__.py
@@ -2,7 +2,8 @@ import os
 from pathlib import Path
 
 __version__ = "0.1.0"
-__git_sha__ = "ca7e4d2"  # updated automatically by pre-commit hook
+__git_sha__ = "a396918"  # updated automatically by pre-commit hook
+__git_date__ = "2026-03-22"  # updated automatically by pre-commit hook
 
 # Global data directory setting
 DEFAULT_DATA_DIR = Path.home() / ".map_plane" / "data"
