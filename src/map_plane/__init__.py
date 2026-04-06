dda4c61376ee3afc9ca080482fe53262c263b06c
rachelicr <rachel.alcraft@icr.ac.uk>
2026-04-06 16:46:37 +0100

    Updated toml

--- src/map_plane/__init__.py
+++ src/map_plane/__init__.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 
 __version__ = "0.1.0"
-__git_sha__ = "f78d388"  # updated automatically by pre-commit hook
-__git_datetime__ = "06-04-2026 16:44"  # updated automatically by pre-commit hook
+__git_sha__ = "e4f5ccd"  # updated automatically by pre-commit hook
+__git_datetime__ = "06-04-2026 16:46"  # updated automatically by pre-commit hook
 
 def version():
     return f"{__version__}+{__git_sha__} ({__git_datetime__})"
