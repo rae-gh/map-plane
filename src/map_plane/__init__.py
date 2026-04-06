5135981d2e3b2390fee7ca3c240036cce2bee16e
rachelicr <rachel.alcraft@icr.ac.uk>
2026-04-06 16:47:17 +0100

    toml quote error

--- src/map_plane/__init__.py
+++ src/map_plane/__init__.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 
 __version__ = "0.1.0"
-__git_sha__ = "e4f5ccd"  # updated automatically by pre-commit hook
-__git_datetime__ = "06-04-2026 16:46"  # updated automatically by pre-commit hook
+__git_sha__ = "dda4c61"  # updated automatically by pre-commit hook
+__git_datetime__ = "06-04-2026 16:47"  # updated automatically by pre-commit hook
 
 def version():
     return f"{__version__}+{__git_sha__} ({__git_datetime__})"
