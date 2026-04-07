6789e0301a2d6ce16452da20ba0ffd3062190b38
rachelicr <rachel.alcraft@icr.ac.uk>
2026-04-07 09:24:18 +0100

    Add pydssp to test profile

--- src/map_plane/__init__.py
+++ src/map_plane/__init__.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 
 __version__ = "0.1.0"
-__git_sha__ = "7fe6a48"  # updated automatically by pre-commit hook
-__git_datetime__ = "07-04-2026 09:20"  # updated automatically by pre-commit hook
+__git_sha__ = "2d0408b"  # updated automatically by pre-commit hook
+__git_datetime__ = "07-04-2026 09:24"  # updated automatically by pre-commit hook
 
 def version():
     return f"{__version__}+{__git_sha__} ({__git_datetime__})"
